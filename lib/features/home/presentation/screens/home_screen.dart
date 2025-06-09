a8606d1aa926a95c760993cdeadfc0e930cbc65c
rahulkandel7 <rahulkandel7@gmail.com>
2025-06-09 09:39:09 +0545

    added list to show assets

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -36,15 +36,12 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text(
-              'Hello, ${user.fullname}',
-              style: TextStyle(fontSize: 26),
-            ),
-          ),
+      appBar: AppBar(
+        title: Text(
+          'Hello, ${user.fullname}',
+          style: TextStyle(fontSize: 26),
+        ),
+        actions: [
           BlocConsumer<AuthBloc, AuthState>(
             listener: (context, state) {
               if (state is LogoutSuccessState) {
@@ -63,17 +60,65 @@ class _HomeScreenState extends State<HomeScreen> {
               }
             },
             builder: (context, state) {
-              return FilledButton(
+              return IconButton.filledTonal(
                 onPressed: state is LogoutLoadingState
                     ? null
                     : () {
                         context.read<AuthBloc>().add(LogoutEvent());
                       },
-                child: Text('Logout'),
+                icon: Icon(Icons.logout),
               );
             },
           ),
         ],
+      ),
+      body: InkWell(
+        onTap: () {},
+        onLongPress: () {},
+        child: Container(
+          // width: 40,
+          // height: 30,
+          margin: EdgeInsets.all(2),
+          decoration: BoxDecoration(
+            color: Colors.grey.shade400,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // CachedNetworkImage(
+              //   imageUrl: '${ApiConstants.imageUrl}$photopath',
+              //   placeholder: (context, url) =>
+              //       Image.asset('assets/logo/logo.png'),
+              //   errorWidget: (context, url, error) =>
+              //       Image.asset('assets/logo/logo.png'),
+              //   height: size.height * 0.14,
+              //   width: double.infinity,
+              //   fit: BoxFit.contain,
+              // ),
+              SizedBox(
+                width: 120,
+                height: 100,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "name",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey.shade900,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.small(
+        onPressed: () {},
+        child: Icon(Icons.add),
       ),
     );
   }
